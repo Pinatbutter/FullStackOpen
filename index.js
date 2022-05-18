8f98005a91b00eb199651d7712bb764bff73514c
Pinatbutter <ferminmitchael@gmail.com>
2022-05-17 23:22:49 -0600

    uibuildCommit

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
 //#region middleware imports
 require('dotenv').config()
-const express = require('express');
+const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/persons')
 const app = express()
-const baseUrl = '/api/persons';
+const baseUrl = '/api/persons'
 //#endregion
 
 //#region app.use express and morgan(tokens)
 app.use(cors())
-app.use(express.static('build'));
+app.use(express.static('build'))
 app.use(express.json())
 
 morgan.token('newData', function (req) { return req.blah })
-function displayNewData (req, res, next) {
-  Object.keys(req.body).length === 0 ? req.newData = ("") : req.newData = JSON.stringify(req.body)
+function displayNewData(req, res, next) {
+  Object.keys(req.body).length === 0 ? req.newData = ('') : req.newData = JSON.stringify(req.body)
   next()
 }
 app.use(displayNewData)
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :newData'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :newData'))
 //#endregion
 
 app.post(baseUrl, (request, response, next) => {
-  const {name, number} = request.body;
-  if (!name|| !number)return response.status(400).json({error: 'content missing'})
-
-  const person = new Person({name, number});
+  const { name, number } = request.body
+  if (!name || !number) return response.status(400).json({ error: 'content missing' })
+  const person = new Person({ name, number })
   person.save()
-  .then(savedContact => {
-    response.json(savedContact)
-  })
-  .catch(error => next(error))
+    .then(savedContact => {
+      response.json(savedContact)
+    })
+    .catch(error => next(error))
 })
-app.put( `${baseUrl}/:id`, (request, response, next) => {
-  const {name, number} = request.body
+app.put(`${baseUrl}/:id`, (request, response, next) => {
+  const { name, number } = request.body
 
-  Person.findByIdAndUpdate(request.params.id, {name, number}, { new: true, runValidators: true, context: 'query' })
+  Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
     .then(updateContact => {
       response.json(updateContact)
     })
@@ -47,31 +46,31 @@ app.put( `${baseUrl}/:id`, (request, response, next) => {
 
 app.get(baseUrl, (request, response) => {
   Person.find({}).then(contact => {
-    contact === null? response.json("Phonebook is empty") :response.json(contact)
+    contact === null ? response.json('Phonebook is empty') : response.json(contact)
   })
 })
 
-app.get('/info', (request, response) =>{
+app.get('/info', (request, response) => {
   Person.countDocuments({}, function (err, count) {
     const display = `<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`
     response.send(display)
   })
 })
 app.get(`${baseUrl}/:id`, (request, response, next) => {
-    Person.findById(request.params.id)
+  Person.findById(request.params.id)
     .then(contact => {
-      contact?  response.json(contact) : response.status(404).end()
+      contact ? response.json(contact) : response.status(404).end()
     })
     .catch(error => next(error))
 })
 //#endregion
 
-app.delete(`${baseUrl}/:id`, (request, response) => {
+app.delete(`${baseUrl}/:id`, (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-  .then(result => {
-    response.status(204).end()
-  })
-  .catch(error => next(error))
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 //#region error Handlers
@@ -84,7 +83,7 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   }
-  else if(error.name === 'ValidationError'){
+  else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   }
   next(error)
--- models/persons.js
+++ models/persons.js
@@ -1,44 +1,44 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URI)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
-const numValidator = (number) =>{
-  const digits = number.split('-');
+const numValidator = (number) => {
+  const digits = number.split('-')
   if( digits.length !== 2 || !/^\d+$/.test(digits[0])  || !/^\d+$/.test(digits[1]) )
-    return false;
+    return false
   else
-    return true;
+    return true
 }
 const numCodeValidator = (number) => {
-  const digits = number.split('-');
+  const digits = number.split('-')
   if (digits[0].length > 3 || digits[0].length < 2)
-    return false;
+    return false
   else
-    return true;
+    return true
 }
 const validateMany = [
-  {validator: numValidator, msg: "Number should only be comprised of digits and one hyphen(-) to separate them"}
-  ,{validator: numCodeValidator, msg: "Incorrect syntax, first part should have 2 or 3 numbers"}
+  { validator: numValidator, msg: 'Number should only be comprised of digits and one hyphen(-) to separate them' }
+  ,{ validator: numCodeValidator, msg: 'Incorrect syntax, first part should have 2 or 3 numbers' }
 ]
 
 const contactSchema = new mongoose.Schema({
-    name:{
-      type: String,
-      minLength: [3, 'Name must be at least 3 characters long'],
-      required: [true, 'Contact name is required']
-    },
-    number: {
-      type: String, 
-      minLength: [8, 'Numbers minimum length is 8'],
-      validate: validateMany,
-      required: [true, 'Contact number is required']
-    }
+  name:{
+    type: String,
+    minLength: [3, 'Name must be at least 3 characters long'],
+    required: [true, 'Contact name is required']
+  },
+  number: {
+    type: String,
+    minLength: [8, 'Numbers minimum length is 8'],
+    validate: validateMany,
+    required: [true, 'Contact number is required']
+  }
 })
 contactSchema.set('toJSON', {
   transform: (document, returnedObject) => {
